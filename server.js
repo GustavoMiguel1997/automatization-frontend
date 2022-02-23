0cc3a4c4722c6e24cc3509cee1dbe9fd771e80ef
gustavo.silva <gustavo.silva@geofusion.com.br>
2022-02-23 11:05:44 -0300

    modificando procfile e inicialização do server

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('port', process.env.PORT || 3000);
 
-const server = app.listen(app.get('port'), function () {
-  console.log('listening on port ', server.address().port);
-});
+app.listen(port, () => console.log(`App is live on port ${port}!`));
